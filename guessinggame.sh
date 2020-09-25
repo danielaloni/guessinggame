a9a3432b7658b007dea75afb2d69033305a09d98
danielaloni <alonidan@bu.edu>
2020-09-25 15:20:56 -0400

    Fixed the code to handle charecters which are not integers

--- guessinggame.sh
+++ guessinggame.sh
@@ -4,20 +4,21 @@
 # This simple program ask the user to guess the number of 
 # files in the current directory.
 
-# The following variable gives the number of files. 
-# ls -p is adding / to directories. 
-# This is piped into egrep -v / which outputs only files (not directories). 
-# This is piped into wc -l which counts the number of files.
-
 function guessinggame {
 local number_of_files=$(ls -p | egrep -v / | wc -l)
+local positive_integer='^[0-9]$'
 
 echo "Please guess the number of files in this directory:"
 read usr_input
 
-while [[ $usr_input -ne $number_of_files ]]
+
+while [[ ! $usr_input =~ $positive_integer ]] || [[ $usr_input -ne $number_of_files ]] 
 do
-	if [[ $usr_input -gt $number_of_files ]]
+	if [[ ! $usr_input =~ $positive_integer  ]]
+		then
+		echo "This is not a positive integer. It does not make sense.  Pplease try again"
+		read usr_input
+	elif [[ $usr_input -gt $number_of_files ]]
 		then
 		echo "This is too high. Please try again:"
 		read usr_input
@@ -26,14 +27,19 @@ do
 		read usr_input
 	fi
 done
-echo "Congrats! Indeed there are $usr_input files in this directory."
 
+if [[ $usr_input -eq $number_of_files ]]
+then
+echo "Congrats! Indeed there are $usr_input files in this directory."
 echo
 echo "               \|/ ____ \|/       	"
 echo "                @~/ ,. \~@        	"
 echo "               /_( \__/ )_\       	"
 echo "                  \__U_/     		"
 echo
+else 
+echo "Error"
+fi
 }
 
 guessinggame
